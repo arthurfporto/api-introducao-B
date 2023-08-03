c6a6e84ab22ad1bc6715a88b8c99d3cad9d129b2
Arthur Porto <artfporto@gmail.com>
2023-08-03 10:09:33 -0300

    Add rota usuarios

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+import dotenv from "dotenv";
 import express from "express"; // Requisição do pacote do express
+import { selectUsuarios } from "./bd.js";
+
+dotenv.config();
+
 const app = express(); // Instancia o Express
 const port = 3000; // Define a porta
 
@@ -8,6 +13,16 @@ app.get("/", (req, res) => {
     nome: "Arthur Porto", // Substitua pelo seu nome
   });
   console.log("Rota / solicitada");
+});
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
 app.listen(port, () => {
